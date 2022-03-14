ada96e782e5f4a7d06bf394557f4742516364234
orlandobel <40770494+orlandobel@users.noreply.github.com>
2022-03-13 19:21:47 -0600

    reajuste de la vista principal

--- srei/src/views/Home.vue
+++ srei/src/views/Home.vue
@@ -14,7 +14,7 @@
                         <div class="card-header border-0">
                             <!-- Pestañas de tabulación -->
                             <ul class="nav nav-tabs" id="tab-inicio" role="tablist">
-                                <li class="nav-item" role="presentation">
+                                <li class="nav-item active" role="presentation">
                                     <a class="nav-link " id="prestamos-tab" href="#prestamos"
                                         data-bs-toggle="tab" role="tab" aria-controls="prestmos" aria-selected="true" aria-current="prestamos">
                                             Nuevo prestamo
@@ -26,7 +26,7 @@
                                             Usuarios del laboratorio
                                     </a>
                                 </li>
-                                <li class="nav-item active" role="presentation">
+                                <li class="nav-item" role="presentation">
                                     <a class="nav-link" id="bitacora-tab" href="#bitacora"
                                         data-bs-toggle="tab" role="tab" aria-controls="bitacora" aria-selected="false">
                                             Bitácora del día
@@ -38,13 +38,13 @@
                         <div class="card-body tab-wrapper overflow-auto">
                             <!-- Secciones de tabulación -->
                             <div class="tab-content " id="tab-inicio-content">
-                                <div class="tab-pane fade px-2 h-100 mh-100  text-start" id="prestamos" role="tabpanel" aria-labelledby="prestamos-tab">
+                                <div class="tab-pane fade px-2 h-100 mh-100 show active" id="prestamos" role="tabpanel" aria-labelledby="prestamos-tab">
                                     <prestamos-tab @prestamoGenerado="prestamoGenerado()" @erroresPrestamo="erroresPrestamo($event)"/>
                                 </div>
                                 <div class="tab-pane fade px-2 h-100 mh-100" id="usuarios" role="tabpanel" aria-labelledby="usuarios-tab">
                                     <alumnos-laboratorio-tab />
                                 </div>
-                                <div class="tab-pane fade h-100 mh-100 show active" id="bitacora" role="tabpanel" aria-labelledby="bitacora-tab">
+                                <div class="tab-pane fade h-100 mh-100 " id="bitacora" role="tabpanel" aria-labelledby="bitacora-tab">
                                     <bitacora-tab />
                                 </div>
                             </div>
