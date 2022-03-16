be5d3e3436d424079bb11b0613cce1d50c2df3b9
GuillermoBautista <guillermo.bg99@gmail.com>
2022-03-16 17:53:59 -0600

    Cambios en validaciones listos para primer revision

--- srei/src/views/SignUp.vue
+++ srei/src/views/SignUp.vue
@@ -17,7 +17,7 @@
                         </div>
                         <div class="row mb-3">
                             <label for="rfc" class="form-label text-start">RFC</label>
-                            <input type="text" name="rfc" id="rfc" class="form-control" v-model="rfc">
+                            <input type="text" name="rfc" id="rfc" class="form-control" style="text-transform: uppercase" v-model="rfc">
                         </div>
                         <div class="row mb-3">
                             <label for="password" class="form-label text-start">Contraseña</label>
@@ -64,6 +64,16 @@ export default {
             labPick:'',
             errorMsg: '',
             successMsg: '',
+            // patron del RFC, persona moral
+            _rfc_pattern_pm: "^(([A-ZÑ&]{3})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|" +
+                  "(([A-ZÑ&]{3})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|" +
+                  "(([A-ZÑ&]{3})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|" +
+                  "(([A-ZÑ&]{3})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$",
+            // patron del RFC, persona fisica
+            _rfc_pattern_pf: "^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|" +
+                       "(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|" +
+                       "(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|" +
+                       "(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$",
         }
     },
     methods: {
@@ -72,20 +82,24 @@ export default {
             console.log(this.labs)
         },
         
-        checkValues(){
-            
-            console.log(nombre.value)
-            console.log(rfc.value)
-            console.log(password.value)
-            console.log(passwordCheck.value)
-            console.log(labPick.value)
+        ValidaRFC(){
+            console.log("Entre")
+            this.rfc = this.rfc.toUpperCase();
+            if (this.rfc.match(this._rfc_pattern_pm) || this.rfc.match(this._rfc_pattern_pf)){
+                console.log("soy un rfc: "+ this.rfc)
+                    return true;
+                }else {
+                console.log("No soy un rfc: "+ this.rfc)
+                    return false;
+                }
         },
+
         async signIn() {
             this.errorMsg =''
             this.successMsg = ''
             const params = {
                 nombre: this.nombre,
-                usuario: this.rfc,
+                usuario: this.rfc.toUpperCase(),
                 clave: this.password,
                 laboratorio: this.labPick,
                 passwordCheck: this.passwordCheck
@@ -102,6 +116,10 @@ export default {
                     if (params.clave == "" || params.passwordCheck == "") this.errorMsg += "<li>Asegúrese de validar la contraseña previamente inscrita.</li>"
                     if (params.laboratorio == "") this.errorMsg += "<li>Asegúrese de seleccionar un laboratorio.</li>"
                     this.errorMsg += "</ul>"
+                    throw new Error('oops');
+                } else if (!this.ValidaRFC()){
+                    this.errorMsg = "<p><strong>¡Error al registrarse!</strong></p>"
+                    this.errorMsg += "<p>Necesita un RFC valido</p>"
                     throw new Error('oops');
                 }
                 const response = await SignIn(params)
@@ -120,7 +138,6 @@ export default {
                         this.errorMsg += "Recuerde que la contraseña inscrita debe ser la misma en los 2 campos de contraseña.</p>"
                     }
                 }
-                console.log(error)
             }
         },
     },
